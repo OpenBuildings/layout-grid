3c0c235fa9270c50eaf79dca9a2536192ed3fab3
Ivan Kerin <ikerin@gmail.com>
2015-08-05 00:33:32 +0300

    Fix options extend

--- js/layout-grid.js
+++ js/layout-grid.js
@@ -222,34 +222,38 @@
         this.compact = undefined !== this.$element.data('compact') ? this.$element.data('compact') : true;
 
         this.options = $.extend(
-            {
-                lg: {
-                    gap: 1,
-                    maxWidth: Number.MAX_VALUE,
-                    cols: 4,
-                    aspect: 2/3
-                },
-                md: {
-                    gap: 2,
-                    maxWidth: 1200,
-                    cols: 3,
-                    aspect: 2/3
-                },
-                sm: {
-                    gap: 3,
-                    maxWidth: 992,
-                    cols: 2,
-                    aspect: 2/3
-                },
-                xs: {
-                    gap: 4,
-                    maxWidth: 768,
-                    cols: 1,
-                    aspect: 2/3
-                },
-            },
+            true,
+            {},
+            LTGrid.DEFAULT_OPTIONS,
             this.$element.data('options') || {}
         );
+    };
+
+    LTGrid.DEFAULT_OPTIONS = {
+        lg: {
+            gap: 1,
+            maxWidth: Number.MAX_VALUE,
+            cols: 4,
+            aspect: 2/3
+        },
+        md: {
+            gap: 2,
+            maxWidth: 1200,
+            cols: 3,
+            aspect: 2/3
+        },
+        sm: {
+            gap: 3,
+            maxWidth: 992,
+            cols: 2,
+            aspect: 2/3
+        },
+        xs: {
+            gap: 4,
+            maxWidth: 768,
+            cols: 1,
+            aspect: 2/3
+        },
     };
 
     /**
